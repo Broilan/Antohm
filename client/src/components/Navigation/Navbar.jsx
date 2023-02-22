235a7b4cc0390b519c3492d24ef990a43c4bcd05
TannerEsch <eschmanntanner@gmail.com>
2023-02-22 12:48:28 -0500

    chore: styled up the signup and login pages, going to make a logo now

--- client/src/components/Navigation/Navbar.jsx
+++ client/src/components/Navigation/Navbar.jsx
@@ -8,29 +8,24 @@ const Navbar = () => {
 
     return (
         <>
+        {isAuthenticated?
             <header>
+                
                 <div className="nav__area">
                     <h1 className="nav__title">Thriver!</h1>
                     
-                    {isAuthenticated?
+                    
                      <>
                     <NavLink to="/" className='nav__link'>Home</NavLink>
                     <NavLink to="/profile" className='nav__link'>Profile</NavLink>
                     <div onClick={handleLogout} className='nav__link'>Logout</div>
                     </>
-                    :
-                    <>
-                    <NavLink to="/login" className='nav__link'>Login</NavLink>
-                    <NavLink to="/signup" className='nav__link'>Signup</NavLink> 
-                    </>
-                    }
-                   
-                    
-                    
-                    
-                    
+
                 </div>
             </header>
+                    :null
+                    
+            }
         </>
     );
 }
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { Navigate, useNavigate } from 'react-router-dom';
+import "../styles/signup.css"
+
 
 const Signup = () => {
     const [name, setName] = useState('');
@@ -12,7 +14,7 @@ const Signup = () => {
     const navigate = useNavigate();
 
     function switchPage() {
-        navigate.push('/login')
+        navigate('/login')
     } 
 
     const handleName = (e) => {
@@ -52,37 +54,76 @@ const Signup = () => {
 
     if (redirect) return <Navigate to="/login" /> // You can have them redirected to profile (your choice)
 
+
     return (
-        <div className="row mt-4">
-            <div className="col-md-7 offset-md-3">
-                <div className="card card-body">
-                    <h2 className="py-2">Signup</h2>
-                    <form onSubmit={handleSubmit}>
-                        <div className="form-group">
-                            <label htmlFor="name">Name</label>
-                            <input type="text" name="name" value={name} onChange={handleName} className="form-control"/>
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="email">Email</label>
-                            <input type="email" name="email" value={email} onChange={handleEmail} className="form-control"/>
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="password">Password</label>
-                            <input type="password" name="password" value={password} onChange={handlePassword} className="form-control"/>
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="confirmPassword">Confirm Password</label>
-                            <input type="password" name="confirmPassword" value={confirmPassword} onChange={handleConfirmPassword} className="form-control"/>
-                        </div>
-                        <input onClick={handleSubmit} type="submit" className="btn btn-primary float-right" value="Submit"/> 
-                        <input onClick={switchPage} type="button" className="btn btn-primary float-right" value="Login"/>                   
+        <div className='h-screen bg-blue-500'>
+
+        <div className=' flex flex-col items-center absolute w-[60%] h-screen'>
+        <p className='absolute text-center text-white font-bold text-[2rem] mt-[5rem]'>Steamline your job search and <br></br>
+                be apart of a  like-minded, supportive <br></br>
+             community that'll help you<br></br> achieve your goals <br></br> and help you stay motivated.</p>
+        </div>            
+
+                    <div className='flex flex-col items-center justify-center  absolute bg-white w-[40%] h-screen right-0'>
+                    <form className="flex flex-col  p-32" onSubmit={handleSubmit}>
+                        <h2 className='text-center mb-10 font-bold text-[3rem]'>Create Account</h2>
+                            <>
+                            <label htmlFor="name" className='font-bold text-[1.5rem]'>Name</label>
+                            <input type="text" name="name" value={name} onChange={handleName} className="border-[1px] mb-6 border-black w-96 h-14 rounded-lg"/>
+                            </>
+
+                            <>
+                            <label htmlFor="email" className='font-bold text-[1.5rem]'>Email</label>
+                            <input type="email" name="email" value={email} onChange={handleEmail} className="border-[1px] mb-6 border-black w-96 h-14 rounded-lg"/>
+                            </>
+
+                            <>
+                            <label htmlFor="password" className='font-bold text-[1.5rem]'>Password</label>
+                            <input type="password" name="password" value={password} onChange={handlePassword} className="border-[1px] mb-6 border-black w-96 h-14 rounded-lg"/>
+                            </>
+
+                            <>
+                            <label htmlFor="confirmPassword" className='font-bold text-[1.5rem]'>Confirm Password</label>
+                            <input type="password" name="confirmPassword" value={confirmPassword} onChange={handleConfirmPassword} className="border-[1px] mb-6 border-black w-96 h-14 rounded-lg"/>
+                            </>
+
+                            <div className='ml-8'>
+                            <input type="checkbox" name="confirmPassword" className='mr-3' />
+                            <label htmlFor="checkbox">I agree to the terms and conditions</label>
+                            </div>
+
+                        <input onClick={handleSubmit} type="submit" className='mt-5 rounded-[10px] bg-blue-500 h-16 font-bold text-white cursor-pointer' value="Create"/> 
+                        <input onClick={switchPage} type="button" className="cursor-pointer" value="Login"/>                   
                         </form>
                          
 
                 </div>
             </div>
-        </div>
     )
 }
 
 export default Signup;
+
+// <div className='h-screen bg-blue-500'>
+// <div className=' flex flex-col items-center absolute w-[60%] h-screen'>
+//     <p className='absolute text-center text-white font-bold text-[2rem] mt-[5rem]'>Steamline your job search and <br></br>be apart of a  like-minded, supportive <br></br>
+//     community that'll help you<br></br> achieve your goals <br></br> and help you stay motivated.</p>
+// </div>  
+//      <div className='flex flex-col items-center justify-center  absolute bg-white w-[40%] h-screen right-0'>
+//         <form className="flex flex-col border-black border-[1px] p-32" onSubmit={handleSubmit}>
+//             <div className='text-center mb-20 font-bold text-[5rem]'>Login</div>
+
+//                 <>
+//                 <label htmlFor="email" className='font-bold text-[1.5rem]'>Email</label>
+//                 <input  className="border-[1px] border-black w-96 h-14 rounded-lg mb-5" type="text" name="email" value={email} onChange={handleEmail} />
+//                 </>
+
+//                 <>
+//                 <label htmlFor="password" className='font-bold text-[1.5rem]'>Password</label>
+//                 <input className="border-[1px] border-black w-96 h-14 rounded-lg" type="text" name="password" value={password} onChange={handlePassword} />
+//                 </>
+//             <button type="submit" className='mt-5 rounded-[10px] bg-blue-500 h-16 font-bold text-white'>Submit</button>
+//             <input  className="mt-10" onClick={switchPage} type="button"value="or sign-up for a free account!"/>            
+//         </form>
+// </div>
+//     </div>
