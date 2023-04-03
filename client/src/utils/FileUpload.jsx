93e06b8dbaea1e54827fd9a5e19260955dbaade9
Tanner Eschmann <107972255+TannerEsch@users.noreply.github.com>
2023-04-03 19:36:45 -0400

    header bugs fixed

--- client/src/utils/FileUpload.jsx
+++ client/src/utils/FileUpload.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
 import axios from 'axios'
 
 export default function handleFile({type, e, userid}) {
+    console.log(type, e, userid)
     return new Promise ((resolve, reject) => {
         const file = e.target.files[0];
         const reader = new FileReader();
@@ -9,10 +9,24 @@ export default function handleFile({type, e, userid}) {
         reader.onloadend = () => {
         const data = {"postType": type, "photo": reader.result }
         if(type == "pfp") {
-                resolve(axios.post(`https://thrive-server.herokuapp.com/upload/${userid}`, data))  
-        }else if(type == "post") {
+                resolve(
+                axios.post(`https://thrive-server.herokuapp.com/upload/${userid}`, data)
+                .then((response) => console.log(response))
+                .catch((err) => console.log(err))
+                )
+            } else if(type == "header") {
+            resolve(
+                axios.post(`https://thrive-server.herokuapp.com/upload/${userid}`, data)
+                .then((response) => console.log(response))
+                .catch((err) => console.log(err))
+            )
+            }else if(type == "post") {
             resolve(reader.result)
-        }
+            console.log('post')
+            }
+            else{
+                reject("Invalid type")
+            }
         }
     })
   }
