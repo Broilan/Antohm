106e9c2c9fa0a91491ca29325bc816d456b89b3a
Tanner Eschmann <107972255+TannerEsch@users.noreply.github.com>
2023-04-04 00:16:43 -0400

    login/signup media queries mostly done

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,5 +1,6 @@
 // Imports
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
+import Signup from './Signup';
 import { Navigate, useNavigate } from 'react-router-dom';
 import { useContext } from 'react';
 import { DataContext } from '../App';
@@ -11,16 +12,10 @@ import logo from '../assets/Thrive.png'
 
 const Login = () => {
     const {currentUser, nowCurrentUser} = useContext(DataContext)
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-
-    const handleEmail = (e) => {
-        setEmail(e.target.value);
-    }
-
-    const handlePassword = (e) => {
-        setPassword(e.target.value);
-    }
+    const loginRef = useRef();
+    const signupRef= useRef();
+    const emailRef = useRef();
+    const passwordRef = useRef();
     const navigate = useNavigate();
 
     function switchPage() {
@@ -29,9 +24,10 @@ const Login = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        const userData = { email, password };
-
-        axios.post(`https://thrive-server.herokuapp.com/user/login`, userData)
+        axios.post(`https://thrive-server.herokuapp.com/user/login`, {
+            email: emailRef.current.value,
+            password: passwordRef.current.value
+        })
         .then(response => {
             const { token } = response.data;
             // save token to localStorage
@@ -49,34 +45,46 @@ const Login = () => {
         });
     }
 
-    if (currentUser) return <Navigate to="/profile" /> // double check
+    if (currentUser) return <Navigate to="/profile" />
 
-    return (   
-        <div className='h-screen  bg-blue-500'>
-            <img src={logo} className="absolute left-[-3rem] overflow-y-hidden h-96 w-96 top-[75%]" />
-            <div className=' flex flex-col items-center absolute w-[60%] h-screen'>
-                <img src={capybara} className=" absolute h-96 w-96 mt-[25rem]"/>
-                <p className='absolute text-center text-white font-bold text-[2rem] mt-[5rem]'>Steamline your job search and <br></br>be apart of a  like-minded, supportive <br></br>
+    return (
+        <>   
+        <div className='flex gap-1 lg:flex-col' id='login-signup-stuff'>
+        <div className='h-screen w-[50vw] bg-blue-500 lg:w-screen '>
+            <img src={logo} className="absolute left-[-3rem] h-96 w-96 top-[75%] 3xs:left-[11.5rem] 3xs:top-[78%] 3xs:h-64 3xs:w-64 2xs:left-[-6rem]" />
+            <div className=' flex flex-col items-center absolute w-[60%] h-screen 1.5xl:w-[50%] lg:w-screen'>
+                <img src={capybara} className=" absolute h-96 w-96 mt-[25rem] 3xs:translate-y-[-6rem]"/>
+                <p className='absolute text-center text-white font-bold text-[2rem] 2xs:text-[1.7rem] 3xs:text-[1.5rem] mt-[5rem] 3xs:mt-[3rem]'>Streamline your job search and <br></br>be apart of a  like-minded, supportive <br></br>
                 community that'll help you<br></br> achieve your goals <br></br> and help you stay motivated.</p>
-            </div>  
-                 <div className='flex flex-col items-center justify-center  absolute bg-white w-[40%] h-screen right-0'>
-                    <form className="flex flex-col border-black border-[1px] rounded-lg shadow-2xl p-32" onSubmit={handleSubmit}>
+                <div className='absolute mt-[26rem] flex gap-10'>
+                <button onClick={() => signupRef.current.scrollIntoView({behavior: 'smooth'})} className='hidden 3xs:translate-y-[-6rem] lg:block bg-blue-400 rounded-xl shadow-2xl border-gray-400 border-2 text-center p-1 w-20 text-white font-bold hover:bg-blue-500'>Signup</button>
+                <button onClick={() => loginRef.current.scrollIntoView({behavior: 'smooth'})} className='hidden 3xs:translate-y-[-6rem] lg:block bg-blue-400 rounded-xl shadow-2xl border-gray-400 border-2 text-center w-20 p-1 text-white font-bold hover:bg-blue-500'>Login</button>
+                </div>
+                </div>
+            </div>
+
+            <div className='flex flex-col items-center justify-center bg-white w-[50vw] lg:w-screen'>
+                    <form ref={loginRef} className="flex flex-col items-center justify-center border-black border-[1px] rounded-lg 1.5xl:p-20 shadow-2xl gap-5 p-32 lg:w-screen lg:h-screen" onSubmit={handleSubmit}>
                         <div className='text-center mb-20 font-bold text-[5rem]'>Login</div>
 
-                            <>
+                            <div>
                             <label htmlFor="email" className='font-bold text-[1.5rem]'>Email</label>
-                            <input  className="border-[1px] border-black w-96 h-14 rounded-lg mb-5" type="text" name="email" value={email} onChange={handleEmail} />
-                            </>
+                            <br />
+                            <input ref={emailRef} className="border-[1px] border-black w-96 h-14 rounded-lg mb-5 2xs:w-80" type="text" name="email"  />
+                            </div>
 
-                            <>
+                            <div>
                             <label htmlFor="password" className='font-bold text-[1.5rem]'>Password</label>
-                            <input className="border-[1px] border-black w-96 h-14 rounded-lg" type="text" name="password" value={password} onChange={handlePassword} />
-                            </>
-                        <button type="submit" className='mt-5 rounded-[10px] bg-blue-500 h-16 font-bold text-white'>Submit</button>
-                        <input  className="mt-10 cursor-pointer" onClick={switchPage} type="button"value="or sign-up for a free account!"/>            
+                            <br />
+                            <input ref={passwordRef} className="border-[1px] border-black w-96 h-14 rounded-lg 2xs:w-80" type="text" name="password"  />
+                            </div>
+                        <button type="submit" className='mt-5 rounded-[10px] bg-blue-500 px-32 h-16 font-bold text-white'>Submit</button>
+                        <input  className="mt-10 cursor-pointer lg:hidden" onClick={switchPage} type="button"value="or sign-up for a free account!"/>            
                     </form>
+                    <div ref={signupRef} className='hidden lg:block'><Signup ref={loginRef} /></div>
+                     </div>
             </div>
-                </div>
+             </>   
     )
 }
 
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { forwardRef, useRef, useState } from 'react';
 import axios from 'axios';
 import { Navigate, useNavigate } from 'react-router-dom';
 import capybara from '../assets/Capybara.png'
@@ -6,137 +6,108 @@ import logo from '../assets/Thrive.png'
 import defaultpfp from '../assets/defaultpfp.png'
 import defaultheader from '../assets/defaultheader.jpg'
 
-const Signup = () => {
-    const [name, setName] = useState('');
-    const [displayName, setDisplayName] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
+const Signup = (props, ref) => {
+    const nameRef = useRef();
+    const displayNameRef = useRef();
+    const emailRef = useRef();
+    const passwordRef = useRef();
+    const confirmPasswordRef = useRef();
     const [redirect, setRedirect] = useState(false);
 
     const navigate = useNavigate();
-
-    function switchPage() {
-        navigate('/login')
-    } 
-
-    const handleName = (e) => {
-         setName(e.target.value);
-    }     
-
-    const handleDisplayName = (e) => {
-        setDisplayName(e.target.value);
-   }  
-
-    const handleEmail = (e) => {
-        setEmail(e.target.value);
-    }
-
-    const handlePassword = (e) => {
-        setPassword(e.target.value);
-    }
-
-    const handleConfirmPassword = (e) => {
-        setConfirmPassword(e.target.value);
-    }
 
     const handleSubmit = (e) => {
         e.preventDefault(); // at the beginning of a submit function
         // make sure password and confirm password are equal
         // password length >= 8 characters
-        if (password === confirmPassword && password.length >= 8) {
+        if (passwordRef.current.value === confirmPasswordRef.current.value && passwordRef.current.value.length >= 8) {
             const pfp = defaultpfp
             const header = defaultheader
-            const newUser = { name, displayName, email, password, pfp, header };
-            axios.post(`https://thrive-server.herokuapp.com/user/signup`, newUser)
-            .then(() => {
-                setRedirect(true);
+            axios.post(`https://thrive-server.herokuapp.com/user/signup`, {
+                name: nameRef.current.value,
+                displayName: displayNameRef.current.value,
+                email: emailRef.current.value,
+                password: passwordRef.current.value,
+                pfp: pfp,
+                header: header
             })
+            .then(() => {
+                if(window.location.pathname === '/signup'){
+                    setRedirect(true);
+                } else {
+                    ref.current.scrollIntoView({behavior: 'smooth'});
+                }
+            }) 
             .catch(error => console.log('===> Error in Signup', error));
         } else {
-            if (password !== confirmPassword) return alert('Passwords don\'t match');
+            if (passwordRef.current.value !== confirmPasswordRef.current.value) return alert('Passwords don\'t match');
             alert('Password needs to be at least 8 characters. Please try again.');
         }
     }
 
-    if (redirect) return <Navigate to="/login" /> // You can have them redirected to profile (your choice)
+    if (redirect) return <Navigate to="/login" />
 
 
     return (
-        <div className='h-screen bg-blue-500'>
+        <>
+        <div className='h-screen lg:hidden bg-blue-500'>
          <img src={logo} className="absolute left-[-3rem] overflow-hidden h-96 w-96 top-[75%]" />
         <div className=' flex flex-col items-center absolute w-[60%] h-screen'>
         <img src={capybara} className=" absolute h-96 w-96 mt-[25rem]"/>
-        <p className='absolute text-center text-white font-bold text-[2rem] mt-[5rem]'>Steamline your job search and <br></br>
+        <p className='absolute text-center text-white font-bold text-[2rem] mt-[5rem]'>Streamline your job search and <br></br>
                 be apart of a  like-minded, supportive <br></br>
              community that'll help you<br></br> achieve your goals <br></br> and help you stay motivated.</p>
-        </div>            
+        </div>  
+        </div>          
 
-                    <div className='flex flex-col items-center justify-center  absolute bg-white w-[40%] h-screen right-0'>
-                    <form className="flex flex-col  p-32" onSubmit={handleSubmit}>
-                        <h2 className='text-center mb-10 font-bold text-[3rem]'>Create Account</h2>
-                            <>
+                    <div className='flex flex-col items-center justify-center absolute top-0 bg-white w-[40%] right-0 lg:relative h-screen lg:w-screen 3xs:top-[9.5rem] '>
+                    <form className="flex flex-col items-center border-black border-[1px] rounded-lg shadow-2xl lg:border-[1px] lg:border-black 3xl:border-0 p-[8rem] mx-8 2xl:px-12 py-[2rem] 3xs:h-fit lg:w-screen lg:h-screen" onSubmit={handleSubmit}>
+                        <h2 className='text-center mb-10 font-bold text-[3rem] mr-7'>Create Account</h2>
+                            <div>
                             <label htmlFor="name" className='font-bold text-[1.5rem]'>Name</label>
-                            <input type="text" name="name" value={name} onChange={handleName} className="border-[1px] mb-6 border-black w-96 h-14 rounded-lg"/>
-                            </>
+                            <br />
+                            <input type="text" name="name" ref={nameRef} className="border-[1px] 2xs:w-80 mb-6 border-black w-96 h-14 rounded-lg"/>
+                            </div>
 
-                            <>
+                            <div>
                             <label htmlFor="display name" className='font-bold text-[1.5rem]'>Display Name</label>
-                            <input type="text" name="name" placeholder='This cant be changed in the future!' value={displayName} onChange={handleDisplayName} className="border-[1px] p-1 mb-6 border-black w-96 h-14 rounded-lg"/>
-                            </>
+                            <br />
+                            <input type="text" name="name" placeholder='This cant be changed in the future!' ref={displayNameRef} className="border-[1px] 2xs:w-80 p-1 mb-6 border-black w-96 h-14 rounded-lg"/>
+                            </div>
 
-                            <>
+                            <div>
                             <label htmlFor="email" className='font-bold text-[1.5rem]'>Email</label>
-                            <input type="email" name="email" value={email} onChange={handleEmail} className="border-[1px] mb-6 border-black w-96 h-14 rounded-lg"/>
-                            </>
+                            <br />
+                            <input type="email" name="email" ref={emailRef} className="border-[1px] mb-6 border-black 2xs:w-80 w-96 h-14 rounded-lg"/>
+                            </div>
 
-                            <>
+                            <div>
                             <label htmlFor="password" className='font-bold text-[1.5rem]'>Password</label>
-                            <input type="password" name="password" value={password} onChange={handlePassword} className="border-[1px] mb-6 border-black w-96 h-14 rounded-lg"/>
-                            </>
+                            <br />
+                            <input type="password" name="password" ref={passwordRef} className="border-[1px] mb-6 2xs:w-80 border-black w-96 h-14 rounded-lg"/>
+                            </div>
 
-                            <>
+                            <div>
                             <label htmlFor="confirmPassword" className='font-bold text-[1.5rem]'>Confirm Password</label>
-                            <input type="password" name="confirmPassword" value={confirmPassword} onChange={handleConfirmPassword} className="border-[1px] mb-6 border-black w-96 h-14 rounded-lg"/>
-                            </>
+                            <br />
+                            <input type="password" name="confirmPassword" ref={confirmPasswordRef} className="border-[1px] 2xs:w-80 mb-6 border-black w-96 h-14 rounded-lg"/>
+                            </div>
 
-                            <div className='ml-8'>
+                            <div className='ml-8 lg:flex lg:ml-4'>
                             <input type="checkbox" name="confirmPassword" className='mr-3' />
+                            <br />
                             <label htmlFor="checkbox">I agree to the terms and conditions</label>
                             </div>
 
-                        <input onClick={handleSubmit} type="submit" className='mt-5 rounded-[10px] bg-blue-500 h-16 font-bold text-white cursor-pointer' value="Create"/> 
-                        <input onClick={switchPage} type="button" className="cursor-pointer" value="Login"/>                   
+                        <input onClick={handleSubmit} type="submit" className='mt-5 rounded-[10px] bg-blue-500 h-16 px-32 mr-6 lg:mr-0 font-bold text-white cursor-pointer' value="Create"/> 
+                        <input onClick={() => navigate('/login')} type="button" className=" lg:hidden cursor-pointer mr-4" value="Login"/>                   
                         </form>
                          
 
                 </div>
-            </div>
+            </>
     )
 }
 
-export default Signup;
+export default forwardRef(Signup);
 
-// <div className='h-screen bg-blue-500'>
-// <div className=' flex flex-col items-center absolute w-[60%] h-screen'>
-//     <p className='absolute text-center text-white font-bold text-[2rem] mt-[5rem]'>Steamline your job search and <br></br>be apart of a  like-minded, supportive <br></br>
-//     community that'll help you<br></br> achieve your goals <br></br> and help you stay motivated.</p>
-// </div>  
-//      <div className='flex flex-col items-center justify-center  absolute bg-white w-[40%] h-screen right-0'>
-//         <form className="flex flex-col border-black border-[1px] p-32" onSubmit={handleSubmit}>
-//             <div className='text-center mb-20 font-bold text-[5rem]'>Login</div>
-
-//                 <>
-//                 <label htmlFor="email" className='font-bold text-[1.5rem]'>Email</label>
-//                 <input  className="border-[1px] border-black w-96 h-14 rounded-lg mb-5" type="text" name="email" value={email} onChange={handleEmail} />
-//                 </>
-
-//                 <>
-//                 <label htmlFor="password" className='font-bold text-[1.5rem]'>Password</label>
-//                 <input className="border-[1px] border-black w-96 h-14 rounded-lg" type="text" name="password" value={password} onChange={handlePassword} />
-//                 </>
-//             <button type="submit" className='mt-5 rounded-[10px] bg-blue-500 h-16 font-bold text-white'>Submit</button>
-//             <input  className="mt-10" onClick={switchPage} type="button"value="or sign-up for a free account!"/>            
-//         </form>
-// </div>
-//     </div>
